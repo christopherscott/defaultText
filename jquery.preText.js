0e91fcf72772eb1e707374905b84481b864f35a5
Christopher Scott Hernandez <c.scott.hernandez@gmail.com>
2010-07-13 18:58:56 -0400

    added support for default values; reorganized stuff

--- jquery.preText.js
+++ jquery.preText.js
@@ -12,45 +12,54 @@ Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.p
 	
 */
 
-	jQuery.fn.preText = function(options) {
+jQuery.fn.preText = function(options) {
 	 	
-	 		var settings = $.extend({
-	 			wrap : true,
-	 			color : "#888",
-	 			defaultText : "default text",
-	 			innerTag : "span",
-	 			outerTag : "div",
-	 			inSpeed : 150,
-	 			outSpeed : 150
-	 		},
+	var settings = $.extend({
+		wrap : true,
+	 	color : "#888",
+	 	defaultText : "default text",
+	 	innerTag : "span",
+	 	outerTag : "div",
+	 	inSpeed : 150,
+	 	outSpeed : 150
+	}, options);
+	
+	
+	
+	return this.each(function() {
 	 		
-	 		options);
-	 	
-	 		return this.each(function() {
+		var $this = $(this),
+	 		pretext = this.defaultValue || $this.attr("alt") || settings.defaultText,
+	 		offsetTop = parseInt($this.css("paddingTop")) + parseInt($this.css("borderTopWidth")),
+	 		offsetLeft = parseInt($this.css("paddingLeft")) + parseInt($this.css("borderLeftWidth")),
+	 		wrapper = $("<" + settings.outerTag + "></" + settings.outerTag + ">")
+	 			.css({"position" : "relative"}),
+	 		overlay = $("<" + settings.innerTag + "></" + settings.innerTag + ">")
+	 			.text(pretext)
+	 			.css({	
+	 				"position" : "absolute",
+	 				"top" :  offsetTop + "px" ,
+	 				"left" : offsetLeft + "px",
+	 				"font-family" : $this.css("font-family"),
+	 				"font-size" : $this.css("font-size"),
+	 				"color" : settings.color
+	 				});
 	 		
-	 			var $this = $(this),
-	 				pretext = this.defaultValue || $this.attr("alt") || settings.defaultText,
-	 				overlay = $("<" + settings.innerTag + "></" + settings.innerTag + ">").text(pretext)
-	 					.css({
-	 						"position" : "absolute",
-	 						"top" : parseInt($this.css("borderTopWidth")) + parseInt($this.css("paddingTop")) + "px" ,
-	 						"left" : parseInt($this.css("borderLeftWidth")) + parseInt($this.css("paddingLeft")) + "px",
-	 						"font-family" : $this.css("font-family"),
-	 						"font-size" : $this.css("font-size"),
-	 						"color" : settings.color
-	 					}),
-	 				wrapper = $("<" + settings.outerTag + "></" + settings.outerTag + ">")
-	 					.css({"position" : "relative"});
-	 			
+		 			
 	 			$this.wrap(wrapper).after(overlay);
+				
+				// if defaultValue is used, go ahead and remove it
+				if ($this.val() == this.defaultValue) {
+	 				$this.val('');
+	 			}
 				
 				// in case of refresh, and values are still there
 				if ($this.val()) { 
 					overlay.css({ "opacity" : "0" });
-				}
-	 				 			
+				}	 				 		
+	 				 		
 	 			$this.focus(function() {
-	 					if ( $this.val() == '' || $this.val() == this.defaultValue ) {
+	 					if ( $this.val() == '' ) {
 	 						overlay.animate({ "opacity" : ".4" }, settings.outSpeed)
 	 					};
 	 				})
@@ -61,8 +70,8 @@ Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.p
 	 					};
 	 				})
 	 				.keypress(function() {
-	 						overlay.animate({ "opacity" : "0"}, settings.outSpeed)
-	 					});
+	 					overlay.animate({ "opacity" : "0"}, settings.outSpeed)
+	 				});
 	 					 			
 	 		});
 	 		
